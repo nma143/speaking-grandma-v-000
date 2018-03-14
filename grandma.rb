f7c0e36120342f845819df4a41a8b813120e677b
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-14 21:34:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 def speak_to_grandma(phrase)
     if phrase==phrase.upcase
       if phrase=="I LOVE YOU GRANDMA!"
-        puts "I LOVE YOU TOO PUMPKIN!"
+        return "I LOVE YOU TOO PUMPKIN!"
       else
-        puts "NO, NOT SINCE 1938!"
+        return "NO, NOT SINCE 1938!"
       end
     else
-      puts "HUH?! SPEAK UP, SONNY!"
+      return "HUH?! SPEAK UP, SONNY!"
     end
 end
